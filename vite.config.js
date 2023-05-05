449fc675492fa19edc4fbfa78c1562f124922a3a
Israel Reyes <li_israelreyes@unca.edu.mx>
2023-05-04 21:36:40 -0600

    First commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://IsraelReyesGarcia.github.io/react-rick-and-morty/",
+  base: "https://github.com/IsraelReyesGarcia/react-rick-and-morty",
   plugins: [react()],
 })
